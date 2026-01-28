752716eb31aa796bf2cc99e5a3e8c767b755007a
Jakub Grzybowski <jakub.grzybowskipl@gmail.com>
2026-01-28 12:35:55 +0100

    template

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d99f60e7c6b4605b0c9d586b7b068920a8dda88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af46aa89705b0e56d2b284cdc15e7b7022d1508")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
